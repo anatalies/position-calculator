b4ece36daf0956dfe0e437494216782cb9ad597b
aromi ben <158552866+benardaromi@users.noreply.github.com>
2024-10-03 23:18:14 +0000

    minor

--- v1/components/TradeForm.tsx
+++ v1/components/TradeForm.tsx
@@ -1,39 +1,11 @@
 'use client'
 import { toast } from "@/hooks/use-toast"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
 import { Input } from "./ui/input"
-import { Button } from "./ui/button"
 import { recordTrade } from "@/lib/actions"
 import { Label } from "./ui/label"
 import SubmitButton from "./SubmitButton"
 
-
- const FormSchema = z.object({
-    lotSize: z.coerce
-      .number({
-        required_error: "Please insert value.",
-      }),
-    profitLoss: z.coerce
-      .number({
-        required_error: "Please insert value.",
-      }),
-    pair: z
-      .string({
-        required_error: "Please select currency.",
-      }),
-    entryPrice: z.coerce
-      .number({
-        required_error: "Please insert value.",
-      }),
-    exitPrice: z.coerce
-      .number({
-        required_error: "Please insert value.",
-      })  
-})
 
 export default function TradeForm() {  
   return (
@@ -48,13 +20,27 @@ export default function TradeForm() {
             <option value={'USD/CHF'}>USD/CHF</option>
           </select>
         </div>
-        <div>
-          <Label>Lot Size</Label>
-          <Input name='lotSize' type="number" step={'0.02'} required/>
+        <div className="grid items-center">
+          <Label>Order Type</Label>
+          <select name="orderType">
+            <option value={'buy'}>Buy</option>
+            <option value={'sell'}>Sell</option>
+          </select>
+        </div>
+        <div className="grid items-center">
+          <Label>Trade Result</Label>
+          <select name="tradeResult">
+            <option value={'profit'}>Profit</option>
+            <option value={'loss'}>Loss</option>
+          </select>
         </div>
         <div>
-          <Label>Profit</Label>
-          <Input name='profit' type="number" required step={'0.10'}/>
+          <Label>Lot Size</Label>
+          <Input name='lotSize' type="number" step={'0.01'} required/>
+        </div>
+        <div>
+          <Label>Amount(USD)</Label>
+          <Input name='amount' type="number" step={'0.01'} required />
         </div>
         <div>
           <Label>Entry Price</Label>
--- v1/next.config.mjs
+++ v1/next.config.mjs
@@ -3,7 +3,7 @@
 const nextConfig = {
     experimental: {
         serverActions: {
-        allowedOrigins: ['https://animated-halibut-pjjxp4px655vh76g4-3000.app.github.dev/', '127.0.0.1', 'localhost:3000'],
+        allowedOrigins: ['https://animated-halibut-pjjxp4px655vh76g4-3000.app.github.dev/', '127.0.0.1', 'localhost:3000', "https://animated-halibut-pjjxp4px655vh76g4-3001.app.github.dev/", 'localhost:3001'],
         },
     },
 };
